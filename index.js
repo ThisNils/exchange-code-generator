4858088b1ef0c6900decf32d0a77fedcfd857764
ThisNils <ThisNils@github.com>
2021-02-09 01:08:20 +0100

    Removed puppeteer, now using device codes

--- index.js
+++ index.js
@@ -1,181 +1,173 @@
-/* eslint-disable camelcase */
-/* eslint-disable object-curly-newline */
-/* eslint-disable prefer-template */
-/* eslint-disable arrow-body-style */
-/* eslint-disable no-empty */
-/* eslint-disable no-console */
-/* eslint-disable no-underscore-dangle */
-const puppeteer = require('puppeteer-core');
-const { post, get } = require('request-promise');
-const { createInterface } = require('readline');
-const { readdir, mkdir, writeFile, readFile } = require('fs').promises;
+const axios = require('axios').default;
+const { createInterface, emitKeypressEvents } = require('readline');
+const {
+  readdir, mkdir, writeFile, readFile,
+} = require('fs').promises;
 const { join } = require('path');
+const { write } = require('clipboardy');
+const open = require('open');
 
 const makeBool = (text) => text.toLowerCase() === 'y' || text.toLowerCase() === 'yes' || !text;
 
-const consoleQuestion = (question, isYN = false) => {
-  return new Promise((res) => {
-    const itf = createInterface(process.stdin, process.stdout);
-    itf.question(isYN ? `${question}(yes/no) ` : question, (answer) => {
-      res(isYN ? makeBool(answer) : answer);
-      itf.close();
-    });
+const consoleQuestion = (question, isYN = false) => new Promise((res) => {
+  const itf = createInterface(process.stdin, process.stdout);
+  itf.question(isYN ? `${question}(yes/no) ` : question, (answer) => {
+    res(isYN ? makeBool(answer) : answer);
+    itf.close();
   });
+});
+
+const makeForm = (keyValuePairs) => {
+  const data = new URLSearchParams();
+  Object.keys(keyValuePairs).forEach((key) => data.append(key, keyValuePairs[key]));
+  return data.toString();
 };
-const wait = (time) => new Promise((res) => setTimeout(res, time));
 
 const useDeviceAuth = async (deviceAuth) => {
-  const { access_token } = await post({
+  const { data: { access_token: accessToken } } = await axios({
+    method: 'POST',
     url: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       Authorization: 'basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=',
     },
-    form: {
+    data: makeForm({
       grant_type: 'device_auth',
       account_id: deviceAuth.accountId,
       device_id: deviceAuth.deviceId,
       secret: deviceAuth.secret,
-    },
-    json: true,
+    }),
   });
-  return get({
+  return (await axios({
     url: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/exchange',
     headers: {
-      Authorization: `bearer ${access_token}`,
+      Authorization: `bearer ${accessToken}`,
     },
-    json: true,
-  });
+  })).data;
 };
 
 const generateDeviceAuth = async (exchangeCode) => {
-  const iosToken = await post({
+  const { data: { access_token: accessToken, account_id: accountId } } = await axios({
+    method: 'POST',
     url: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       Authorization: 'basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=',
     },
-    form: {
+    data: makeForm({
       grant_type: 'exchange_code',
       exchange_code: exchangeCode,
       includePerms: false,
-    },
-    json: true,
+    }),
   });
-  return post({
-    url: `https://account-public-service-prod.ol.epicgames.com/account/api/public/account/${iosToken.account_id}/deviceAuth`,
+
+  const { data: { deviceId, secret } } = await axios({
+    method: 'POST',
+    url: `https://account-public-service-prod.ol.epicgames.com/account/api/public/account/${accountId}/deviceAuth`,
     headers: {
-      Authorization: `bearer ${iosToken.access_token}`,
+      Authorization: `bearer ${accessToken}`,
     },
-    json: true,
   });
+  return { accountId, deviceId, secret };
 };
 
+const getDeviceCode = async () => {
+  const { data: { access_token: switchAccessToken } } = await axios({
+    method: 'POST',
+    url: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'basic NTIyOWRjZDNhYzM4NDUyMDhiNDk2NjQ5MDkyZjI1MWI6ZTNiZDJkM2UtYmY4Yy00ODU3LTllN2QtZjNkOTQ3ZDIyMGM3=',
+    },
+    data: makeForm({
+      grant_type: 'client_credentials',
+    }),
+  });
+  const { data: { verification_uri_complete: url, device_code: deviceCode } } = await axios({
+    method: 'POST',
+    url: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/deviceAuthorization',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `bearer ${switchAccessToken}`,
+    },
+    data: makeForm({
+      prompt: 'login',
+    }),
+  });
+  return { url, deviceCode };
+};
+
+const useDeviceCode = (deviceCode) => new Promise((res) => {
+  let retries = 0;
+  const requestInterval = setInterval(async () => {
+    try {
+      const { data: { access_token: accessToken } } = await axios({
+        method: 'POST',
+        url: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'basic NTIyOWRjZDNhYzM4NDUyMDhiNDk2NjQ5MDkyZjI1MWI6ZTNiZDJkM2UtYmY4Yy00ODU3LTllN2QtZjNkOTQ3ZDIyMGM3=',
+        },
+        data: makeForm({
+          grant_type: 'device_code',
+          device_code: deviceCode,
+        }),
+      });
+      clearInterval(requestInterval);
+      const { data: { code: exchangeCode } } = await axios({
+        url: 'https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/exchange',
+        headers: {
+          Authorization: `bearer ${accessToken}`,
+        },
+      });
+      res(exchangeCode);
+    } catch (err) {
+      if (retries >= 24) {
+        clearInterval(requestInterval);
+        res();
+      } else retries += 1;
+    }
+  }, 10000);
+});
+
 (async () => {
-  const savingFolder = process.env.APPDATA || (process.platform === 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + '/.local/share');
+  delete axios.defaults.headers.post['Content-Type'];
+  const savingFolder = process.env.APPDATA || (process.platform === 'darwin' ? `${process.env.HOME}/Library/Preferences` : `${process.env.HOME}/.local/share`);
   const userFolders = await readdir(savingFolder);
   if (!userFolders.find((f) => f === 'ecg')) await mkdir(join(savingFolder, 'ecg'));
   const savedFiles = await readdir(join(savingFolder, 'ecg'));
-  if (savedFiles.find((sv) => sv === 'deviceauth')) {
-    if (await consoleQuestion('Found a saved profile! Do you want to use it? ', true)) {
-      const deviceAuthCredentials = JSON.parse(await readFile(join(savingFolder, 'ecg') + '/deviceauth'));
-      const { code } = await useDeviceAuth(deviceAuthCredentials);
-      console.log(`Your exchange code is: ${code}`);
-      console.log('This terminal will be closed in 15 seconds');
-      await wait(15000);
-      return;
-    }
-  }
-  console.log('Checking for Chrome installation');
-  let chromeIsAvailable = true;
-  try {
-    if (process.platform !== 'win32') throw new Error();
-    const chromePath = await readdir(`${process.env['ProgramFiles(x86)']}\\Google\\Chrome\\Application`);
-    if (!chromePath.find((f) => f === 'chrome.exe')) throw new Error();
-  } catch (e) {
-    chromeIsAvailable = false;
-  }
 
-  let executablePath;
-  if (chromeIsAvailable) {
-    executablePath = `${process.env['ProgramFiles(x86)']}\\Google\\Chrome\\Application\\chrome.exe`;
-    console.log('Chrome is already installed');
+  let deviceAuth;
+  if (savedFiles.find((sv) => sv === 'deviceauth') && await consoleQuestion('Found a saved profile! Do you want to use it? ', true)) {
+    deviceAuth = JSON.parse(await readFile(join(savingFolder, 'ecg', 'deviceauth')));
   } else {
-    const browserFetcher = puppeteer.createBrowserFetcher({
-      path: join(savingFolder, 'ecg'),
-    });
-    console.log(await browserFetcher.canDownload('666595') ? 'Downloading Chrome. This may take a while!' : 'Chrome is already installed');
-    const downloadInfo = await browserFetcher.download('666595');
-    executablePath = downloadInfo.executablePath;
+    console.log('Setting up login window');
+    const { url, deviceCode } = await getDeviceCode();
+    await open(url);
+    console.log('Please log into your account');
+    const exchangeCode = await useDeviceCode(deviceCode);
+    console.log('Saving login credentials');
+    deviceAuth = await generateDeviceAuth(exchangeCode);
+    await writeFile(join(savingFolder, 'ecg', 'deviceauth'), JSON.stringify(deviceAuth));
   }
-  console.log('Starting chrome...');
-  const browser = await puppeteer.launch({
-    executablePath,
-    headless: false,
-    devtools: false,
-    defaultViewport: {
-      width: 500, height: 800,
-    },
-    args: ['--window-size=500,800', '--lang=en-US'],
+  console.log('Generating exchange code');
+  const { code: exchangeCode } = await useDeviceAuth(deviceAuth);
+  console.log(`\nYour exchange code is: ${exchangeCode}\nYour device auth is ${JSON.stringify(deviceAuth)}\n`);
+  console.log('Press E to copy the exchange code to your clipboard\nPress A to copy the device auth to your clipboard\nThis window will auto close in 30 secs');
+  const itf = createInterface(process.stdin, process.stdout);
+  emitKeypressEvents(process.stdin, itf);
+  process.stdin.setRawMode(true);
+  process.stdin.on('keypress', (str, key) => {
+    process.stdout.cursorTo(0);
+    process.stdout.write('\r\x1b[K');
+    if (key.name.toLowerCase() === 'e') {
+      write(exchangeCode);
+      console.log('exchange code copied to clipboard');
+    } else if (key.name.toLowerCase() === 'a') {
+      write(JSON.stringify(deviceAuth, null, 2));
+      console.log('device auth copied to clipboard');
+    }
   });
-
-  console.log('Chrome started! Please log in');
-
-  const page = await browser.pages().then((p) => p[0]);
-  await page.goto('https://epicgames.com/id');
-  await (await page.waitForSelector('#login-with-epic')).click();
-  await page.waitForRequest((req) => req.url() === 'https://www.epicgames.com/account/personal' && req.method() === 'GET', {
-    timeout: 120000000,
-  });
-
-  const oldXsrfToken = (await page.cookies()).find((c) => c.name === 'XSRF-TOKEN').value;
-  page.once('request', (req) => {
-    req.continue({
-      method: 'GET',
-      headers: {
-        ...req.headers,
-        'X-XSRF-TOKEN': oldXsrfToken,
-      },
-    });
-  });
-  await page.setRequestInterception(true);
-  await page.goto('https://www.epicgames.com/id/api/authenticate');
-  await page.setRequestInterception(false);
-
-  page.once('request', (req) => {
-    req.continue({
-      method: 'GET',
-      headers: {
-        ...req.headers,
-        'X-XSRF-TOKEN': oldXsrfToken,
-      },
-    });
-  });
-  await page.setRequestInterception(true);
-  try {
-    await page.goto('https://www.epicgames.com/id/api/csrf');
-  } catch (e) {}
-  await page.setRequestInterception(false);
-
-  const xsrfToken = (await page.cookies()).find((c) => c.name === 'XSRF-TOKEN').value;
-  page.once('request', (req) => {
-    req.continue({
-      method: 'POST',
-      headers: {
-        ...req.headers,
-        'X-XSRF-TOKEN': xsrfToken,
-      },
-    });
-  });
-  await page.setRequestInterception(true);
-  const pageJSON = await (await page.goto('https://www.epicgames.com/id/api/exchange/generate')).json();
-  await browser.close();
-
-  const deviceAuthCredentials = await generateDeviceAuth(pageJSON.code);
-  await writeFile(join(savingFolder, 'ecg') + '/deviceauth', JSON.stringify(deviceAuthCredentials));
-
-  const { code } = await useDeviceAuth(deviceAuthCredentials);
-  console.log(`Your exchange code is: ${code}`);
-  console.log('This terminal will be closed in 15 seconds');
-  await wait(15000);
+  await new Promise((res) => setTimeout(res, 30000));
+  itf.close();
 })();
